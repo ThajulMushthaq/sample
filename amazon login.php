1154d4a89cd6b658eeea0b8bfd1da78385014780
thajul mushthaq <thajutj056@gmail.com>
2021-08-14 17:20:39 +0530

    form

--- amazon login.php
+++ amazon login.php
@@ -1,13 +1,25 @@
 <html>
 <head>
     <title>Amazone Login</title>
+    <style>
 
+    </style>
 
 </head>
 <body>
     <div>
         <img src="amazon-logo.png" width="250" height="70">
-        
+    </div>
+    <div>
+        <form>
+            <div>
+                E-mail :<input type="text",name="mail"><br>
+                Password :<input type="password",name="pswrd"><br>
+            </div>
+            <div>
+                <input type="submit",name="login",value="Login">
+            </div>
+        </form>
     </div>
 
 

042b8e0d7a9030c7ca7ee904f04fb88a8bbe39a4
thajul mushthaq <thajutj056@gmail.com>
2021-08-14 15:26:46 +0530

    h

---
+++ $server.php
@@ -0,0 +1,13 @@
+<?php
+echo $_SERVER['PHP_SELF'];
+ECHO "<br>";
+echo $_SERVER['GATEWAY_INTERFACE'];
+ECHO "<br>";
+// echo $_SERVER['HTTPS'];
+// ECHO "<br>";
+echo $_SERVER['SERVER_NAME'];
+ECHO "<br>";
+ECHO "<br>";
+ECHO "<br>";
+ECHO "<br>";
+?>
